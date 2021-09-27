aeeeeebf4a613eb6508fa82b37c3bc8fb541f381
Jared Trenholm <Jared.Trenholm12@gmail.com>
2021-09-27 14:03:58 -0300

    Started Pickup Script and Started Coin system
    Started working on Pickups
    
    Pickups required a coin system, so I also started a coin system for the player and so the pickups can function

---
+++ Assets/Scripts/Pickups.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+    public PickupTypes type;
+    public string playerTag = "Player";
+    public GameObject player;
+    public int value;
+    public enum PickupTypes { 
+        Coin,
+        Bill,
+        Food,
+        MysteryMeat
+    }
+
+    private void Collected()
+    {
+        switch (type) {
+            case PickupTypes.Coin:
+                player.GetComponent<PlayerStats>().ObtainCoins(value);
+                break;
+            case PickupTypes.Bill:
+
+                break;
+            case PickupTypes.Food:
+
+                break;
+            case PickupTypes.MysteryMeat:
+
+                break;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == playerTag)
+        {
+            player = other.gameObject;
+            Collected();
+        }
+    }
+}
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -5,4 +5,22 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     public float speedModifier;
+    public int amountOfCoins;
+
+    public void SetCoins(int value)
+    {
+        amountOfCoins = value;
+        if (amountOfCoins < 0)
+            amountOfCoins = 0;
+    }
+    public void ObtainCoins(int value)
+    {
+        amountOfCoins += value;
+    }
+    public void LoseCoins(int value)
+    {
+        amountOfCoins -= value;
+        if (amountOfCoins < 0)
+            amountOfCoins = 0;
+    }
 }
