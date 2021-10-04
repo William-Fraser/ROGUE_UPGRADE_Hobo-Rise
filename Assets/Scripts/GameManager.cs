ebede5047b480e8d061571a1686bb718c28548f2
Jared Trenholm <Jared.Trenholm12@gmail.com>
2021-10-04 12:47:26 -0300

    Saving and Loading Started
    Saving and Loading Started
    There are a few checks in place and the Player will properly handles the stats at the start.
    
    At the start the player finds their stats on the GameManager.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager gameManager;
+    public PlayerData data = new PlayerData();
+    public GameObject player;
+    public bool isNewGame = false;
+    public int mainID;
+    public int inGameID;
+    public int victoryID;
+    public int gameOverID;
+    public int resultsID;
+    public int upgradeID;
+    public int creditsID;
     private GameScenes currentScene;
 
     private enum GameScenes { 
@@ -38,14 +49,72 @@ public class GameManager : MonoBehaviour
     }
     #endregion
 
-    #region Save and Load
-    public void Save()
+    #region Save and Load System
+    public void NewGame()
     {
-
+        isNewGame = true;
+        ChangeScene(GameScenes.Upgrade);
     }
     public void Load()
     {
+        try
+        {
+            if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
 
+                PlayerData loadData = (PlayerData)bf.Deserialize(file);
+                file.Close();
+
+                data.maxHealth = loadData.maxHealth;
+                data.speedModifier = loadData.speedModifier;
+                data.damageModifier = loadData.damageModifier;
+                data.attackSpeedModifier = loadData.attackSpeedModifier;
+                data.maxEnergy = loadData.maxEnergy;
+                data.totalMoney = loadData.totalMoney;
+
+                ChangeScene(GameScenes.Upgrade);
+
+            }
+        }
+        catch
+        {
+            Debug.LogError("THE LOADED SAVE CANNOT BE PARSED CORRECTLY");
+        }
+    }
+    public void AttemptSave()
+    {
+        if (isNewGame == true && CanLoad())
+        {
+            // CREATE SAVING WARNING
+        } else
+        {
+            Save();
+        }
+    }
+    public bool CanLoad()
+    {
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            return true;
+        }
+        return false;
+    }
+    private void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+
+        data.maxHealth = player.GetComponent<Stats>().maxHealth;
+        data.speedModifier = player.GetComponent<Stats>().speedModifier;
+        data.damageModifier = player.GetComponent<Stats>().damageModifier;
+        data.attackSpeedModifier = player.GetComponent<Stats>().attackSpeedModifier;
+        data.maxEnergy = player.GetComponent<Stats>().maxEnergy;
+        data.totalMoney = player.GetComponent<Stats>().totalMoney;
+
+        bf.Serialize(file, data);
+        file.Close();
     }
     #endregion
 
@@ -53,6 +122,43 @@ public class GameManager : MonoBehaviour
     private void ChangeScene(GameScenes targetScene)
     {
         currentScene = targetScene;
+        switch (currentScene) {
+            case GameScenes.Main:
+                SceneManager.LoadScene(mainID);
+                break;
+            case GameScenes.InGame:
+                SceneManager.LoadScene(inGameID);
+                break;
+            case GameScenes.Victory:
+                SceneManager.LoadScene(victoryID);
+                break;
+            case GameScenes.GameOver:
+                SceneManager.LoadScene(gameOverID);
+                break;
+            case GameScenes.Results:
+                SceneManager.LoadScene(resultsID);
+                break;
+            case GameScenes.Upgrade:
+                SceneManager.LoadScene(upgradeID);
+                break;
+            case GameScenes.Credits:
+                SceneManager.LoadScene(creditsID);
+                break;
+            default:
+                SceneManager.LoadScene(upgradeID);
+                break;
+        }
     }
     #endregion
+}
+
+[Serializable]
+public class PlayerData
+{
+    public int maxHealth;
+    public float speedModifier;
+    public float damageModifier;
+    public float attackSpeedModifier;
+    public int maxEnergy;
+    public float totalMoney;
 }
