3e8b2a9046e4d057095e903b6935c101891ff62b
Jared Trenholm <Jared.Trenholm12@gmail.com>
2021-10-17 21:41:09 -0300

    Coin UI

---
+++ Assets/CoinUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinUI : MonoBehaviour
+{
+    public Text coinText;
+    public bool inGame;
+
+
+    private void OnEnable()
+    {
+        if(GameManager.gameManager.currentScene == GameManager.GameScenes.InGame)
+        {
+            inGame = true;
+        } else
+        {
+            inGame = false;
+        }
+    }
+    private void Update()
+    {
+        if (inGame)
+        {
+            coinText.text = "Coins: " + GameManager.gameManager.player.GetComponent<Stats>().collectedMoney;
+        } else
+        {
+            coinText.text = "Coins: " + GameManager.gameManager.stats.totalMoney;
+        }
+    }
+}
