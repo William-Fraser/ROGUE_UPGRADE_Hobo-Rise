232a679b706af98b1c7f7c87899c759c377a9bbd
Jared Trenholm <Jared.Trenholm12@gmail.com>
2021-11-01 13:48:38 -0300

    Better In-Game Stat Indication

--- Assets/Scripts/healthUI.cs
+++ Assets/Scripts/healthUI.cs
@@ -7,8 +7,10 @@ using UnityEngine.UI;
 public class healthUI : MonoBehaviour
 {
     public Image heart;
+    public Text health;
     private void Update()
     {
         heart.rectTransform.localScale = new Vector3(GameManager.gameManager.player.GetComponent<Stats>().health / GameManager.gameManager.player.GetComponent<Stats>().maxHealth, GameManager.gameManager.player.GetComponent<Stats>().health / GameManager.gameManager.player.GetComponent<Stats>().maxHealth, 1);
+        health.text = ""+GameManager.gameManager.player.GetComponent<Stats>().health;
     }
 }
