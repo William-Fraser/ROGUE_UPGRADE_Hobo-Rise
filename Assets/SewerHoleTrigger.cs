e123f514b07369d67b30978c1bec2a90f63ba95d
sir William <williamfr0615@gmail.com>
2021-11-07 15:36:15 -0400

    changes stat balancing, added SewerHole Trigger

---
+++ Assets/SewerHoleTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SewerHoleTrigger : MonoBehaviour
+{
+    public GameObject ground;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            ground.SetActive(false);
+        }
+    }
+}
