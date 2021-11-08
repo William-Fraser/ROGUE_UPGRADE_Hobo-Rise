de0db3a671d8c1f508c56f6fba32bafb76fb3a93
Jared Trenholm <Jared.Trenholm12@gmail.com>
2021-11-08 14:52:40 -0400

    [Added] Cutscene scene and code

---
+++ Assets/Scripts/CutsceneManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class CutsceneManager : MonoBehaviour
+{
+    public GameObject[] textBoxObjects;
+    public int cutsceneProgress = 0;
+
+    private void Update()
+    {
+        if(Input.anyKeyDown == true)
+        {
+            cutsceneProgress += 1;
+            GameManager.gameManager.ButtonPressed();
+        }
+        PlayCutscene();
+    }
+    public void PlayCutscene()
+    {
+        foreach(GameObject cutsceneObject in textBoxObjects)
+        {
+            cutsceneObject.SetActive(false);
+        }
+        if (cutsceneProgress <= textBoxObjects.GetUpperBound(0))
+        {
+            textBoxObjects[cutsceneProgress].SetActive(true);
+        } else
+        {
+            GameManager.gameManager.ChangeScene(GameManager.GameScenes.InGame);
+        }
+    }
+}
