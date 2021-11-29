f495a32da1f1a7f3fbb07bcfcd29b8fe81b56eb3
Jared Trenholm <Jared.Trenholm12@gmail.com>
2021-11-29 15:46:17 -0400

    [Clean] Removed Unused Usings

--- Assets/Scripts/AI/AI_Controller.cs
+++ Assets/Scripts/AI/AI_Controller.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Pathfinding;
+using UnityEngine;
 
 public enum STATE
 { 
--- Assets/Scripts/UI/UpdateUpgradeScreen.cs
+++ Assets/Scripts/UI/UpdateUpgradeScreen.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
