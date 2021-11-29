9dcb0e5cb949dd0837415dd13df3cd5ecdc6c862
Jared Trenholm <Jared.Trenholm12@gmail.com>
2021-11-29 12:35:30 -0400

    [Cleaned] Stats Cleaned

--- Assets/Scripts/Player/Stats.cs
+++ Assets/Scripts/Player/Stats.cs
@@ -6,29 +6,18 @@ public class Stats : MonoBehaviour
     public GameObject topParent;
 
     public bool alive = true;
-    public CharacterType type;
     public float maxHealth;
     public float health;
     public float speedModifier;
-    public float damageModifier;
     public float attackSpeedModifier;
     public float maxEnergy;
     public float energy;
     public int displayedEnergy; // used to display in place of energy to show better looking scale
-    public float collectedMoney; // money collected in a single run
-    public float totalMoney; // total collected money in game
     public SpriteRenderer sprite;
     public AudioClip deathSound;
     public AudioClip takeDamageSound;
     
     public AudioSource audioSource;
-
-    public enum CharacterType { 
-        Player,
-        Hobo,
-        TaxCollector,
-        Police
-    }
     private void Start()
     {
         if (GetComponentInChildren<SpriteRenderer>())
@@ -43,22 +32,17 @@ public class Stats : MonoBehaviour
         { 
             Death();
         }
-
-        if (gameObject.CompareTag(GameManager.gameManager.player.tag))
-        { // why reference the tag and not the player?
-            LoseEnergy(Time.deltaTime);
-            displayedEnergy = Convert.ToInt16(energy);
-            if(energy <= 0)
-            {
-                Death();
-            }
+        LoseEnergy(Time.deltaTime);
+        displayedEnergy = Convert.ToInt16(energy);
+        if(energy <= 0)
+        {
+            Death();
         }
     }
     public void ResetInGameStats()
     {
         health = maxHealth;
         energy = maxEnergy;
-        collectedMoney = 0;
         alive = true;
     }
     public void ResetMainStats()
@@ -66,10 +50,8 @@ public class Stats : MonoBehaviour
         PlayerData stats = GameManager.gameManager.GetPlayerStats();
         maxHealth = stats.maxHealth;
         speedModifier = stats.speedModifier;
-        damageModifier = stats.damageModifier;
         attackSpeedModifier = stats.attackSpeedModifier;
         maxEnergy = stats.maxEnergy;
-        totalMoney = stats.totalMoney;
     }
 
     #region Set, Obtain, and Lose resources
@@ -86,7 +68,7 @@ public class Stats : MonoBehaviour
     }
     public void LoseEnergy(float value)
     {
-        if (GameManager.gameManager.currentScene != GameManager.GameScenes.InGame || health <= 0 || energy <= 0)
+        if (GameManager.gameManager.currentScene != GameManager.GameScenes.InGame || (health <= 0 && energy <= 0))
             return;
         energy -= value;
         if (energy < 0)
@@ -94,7 +76,7 @@ public class Stats : MonoBehaviour
     } 
     public void LoseHealth(int value, bool attack)
     {
-        if (GameManager.gameManager.currentScene != GameManager.GameScenes.InGame || health <= 0)
+        if (GameManager.gameManager.currentScene != GameManager.GameScenes.InGame || (health <= 0 && energy <= 0))
             return;
         if (health - value < 0)
         {
@@ -103,15 +85,9 @@ public class Stats : MonoBehaviour
         else // take damage 
         {
             audioSource.PlayOneShot(takeDamageSound);
-            GameManager.gameManager.DisplayGUIPopup("-"+value, this.transform.position, Color.red);
             health -= value;
-            //Debug.Log($"STATS: {-value}, pos: {this.transform.position}, colour: {Color.red}");
         }
-
-        if (attack)
-        { 
-            /*sprite.color = Color.red;*/
-        }
+        GameManager.gameManager.DisplayGUIPopup("-" + value, this.transform.position, Color.red);
 
         if (health < 0)
             health = 0;
@@ -123,32 +99,7 @@ public class Stats : MonoBehaviour
         if (GameManager.gameManager.currentScene != GameManager.GameScenes.InGame && GameManager.gameManager.currentScene != GameManager.GameScenes.GameOver && GameManager.gameManager.currentScene != GameManager.GameScenes.Results)
             return;
         alive = false;
-        //calculating drop pos / positioning from world scale
-        float itemDropPosY = transform.position.y - (transform.localPosition.y + transform.localPosition.y / 2);
-        //Debug.LogError($" itemDropPosY: {transform.position.y - (transform.localPosition.y / 2)}, topOfSpritePosY: {transform.position.y}, localY {transform.localPosition.y}");
-        Vector3 itemDropPos = new Vector3(transform.position.x, itemDropPosY, transform.position.z);
-
-        switch (type)
-        {
-            case CharacterType.Hobo:
-                CoinManager.coinManager.SpawnBronzeCoin(itemDropPos);
-                topParent.SetActive(false);
-                break;
-            case CharacterType.TaxCollector:
-                CoinManager.coinManager.SpawnGoldCoin(itemDropPos);
-                topParent.SetActive(false);
-                break;
-            case CharacterType.Police:
-                CoinManager.coinManager.SpawnBill(itemDropPos);
-                topParent.SetActive(false);
-                break;
-        }
-       
-
         audioSource.PlayOneShot(deathSound);
-        // Color colour = GetComponent<SpriteRenderer>().color;
-      //  colour.a = 0; 
-       // GetComponent<SpriteRenderer>().color = colour;
     }
 
     private float CheckValue(float value, float maxValue)
@@ -157,9 +108,5 @@ public class Stats : MonoBehaviour
             value = maxValue;
 
         return value;
-
-       
-        
-
     }
 }
